13fee867365b44ab3ed930ef250bc362eb74cc8a
Tu-An Nguyen <t.nhunhu.ng@gmail.com>
2023-12-07 16:10:19 -0800

    Reload page after login/out

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -9,23 +9,25 @@ const Login = () => {
   const [token, setToken] = useState("");
 
   useEffect(() => {
-    const hash = window.location.hash;
-    let token = window.localStorage.getItem("token");
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
 
     if (!token && hash) {
-      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
-      window.location.hash = "";
-      window.localStorage.setItem("token", token);
-    };
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+      window.location.reload()
+    }
     
-    setToken(token);
-  }, []);
+    setToken(token)
+  }, [])
 
   const logout = () => {
-    setToken("");
-    window.localStorage.removeItem("token");
-  };
+    setToken("")
+    window.localStorage.removeItem("token")
+    window.location.reload()
+  }
 
   return (
     <div>
@@ -35,6 +37,6 @@ const Login = () => {
     </div>
   );
 
-};
+}
 
 export default Login
--- src/components/TextBox/index.js
+++ src/components/TextBox/index.js
@@ -1,9 +1,34 @@
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
 const TextBox = () => {
+  let token = window.localStorage.getItem("token");
+  const [searchKey, setSearchKey] = useState("");
+
+  const searchSong = async (e) => {
+    // e.preventDefault();
+    const {data} = await axios.get("https://api.spotify.com/v1/tracks/", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        id: searchKey, // Todo: clean URL for Spotify ID
+      }
+    })
+  }
+  
   return (
-    <form class="my-5">
-      <input class="w-6/12 border rounded-lg p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white" type="url" placeholder="https://open.spotify.com/track/..."></input>
-    </form>
+    <div>
+      {token ?
+        <form class="my-5">
+          <input class="w-6/12 border rounded-lg p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white" type="url" placeholder="https://open.spotify.com/track/..." onChange={e => setSearchKey(e.target.value)}></input>
+          <button class="bg-green-500 text-white font-medium py-2 px-4 m-5 rounded" type={"submit"}>Search</button>
+        </form>
+      
+        : <h2 class="text-white font-medium">Please login</h2>
+      }
+    </div>
   );
-};
+}
 
 export default TextBox;
