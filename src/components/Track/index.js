44f3bc2ba6c8d747f116b44c4023bbb6c40a627d
Tu-An Nguyen <t.nhunhu.ng@gmail.com>
2023-12-12 14:21:00 -0800

    Try updating genres with useEffect

--- src/components/Track/index.js
+++ src/components/Track/index.js
@@ -1,22 +1,109 @@
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import React, {useState, useEffect} from "react";
 import NavBar from "../NavBar";
 
 const Track = () => {
   const token = window.localStorage.getItem("token");
   const { id } = useParams();
-  const genres = [];
+  // let genres = [];
+  const [genres, setGenres] = useState([]);
 
-  const getTrack = async (e) => {
-    const {data} = await axios.get("https://api.spotify.com/v1/tracks/" + id, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    },
-    params: {
-      id: id,
+  useEffect(() => {
+    const getTrack = async () => {
+      // Request Track object from Spotify API
+      const track = await axios.get("https://api.spotify.com/v1/tracks/" + id, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        params: {
+          id: id,
+        }
+        })
+
+      const artists = track.data.artists;
+      const tempGenres = genres
+
+      // Update genres array by requesting Artist object from Spotify API
+      const getGenres = async (artist) => {
+        const artistObject = await axios.get("https://api.spotify.com/v1/artists/" + artist.id, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        params: {
+          id: artist.id,
+        }
+        });
+
+        try {
+          artistObject.data.genres.forEach((g) => {
+            if (!genres.includes(g)) {
+              tempGenres.push(g)
+            }
+          });
+          setGenres(tempGenres)
+        } catch (e) {
+          console.log("No genres found.");
+        }
+      }
+      
+      // Update genres array for each artist on the track
+      for (const artist of artists) {
+        await getGenres(artist);
+      }
     }
-    });
-    
+    console.log(genres);
+
+    getTrack();
+  }, [])
+  
+
+    /*
+        .then(response => {
+          const artists = response.data.artists;
+
+          artists.forEach((artist) => {
+            axios.get("https://api.spotify.com/v1/artists/" + artist.id, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            },
+            params: {
+              id: artist.id,
+            }
+            })
+              .then(response => {
+                const artistObject = response.data;
+                artistObject.genres.forEach((g) => genres.push(g));
+              })
+              .catch(err => {
+                console.error(err);
+              });
+          })
+        })
+        .catch(err => {
+          console.error(err);
+        });
+
+    console.log(track);
+    const artists = track.artists;
+
+    artists.forEach((artist) => {
+      const { artistObject } = axios.get("https://api.spotify.com/v1/artists/" + artist.id, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        id: artist.id,
+      }
+      });
+
+      try {
+        artistObject.genres.forEach((g) => genres.push(g));
+      } catch (e) {
+        console.log("No genres found.");
+      }
+    })
+
     for (let i = 0; i < data.artists.length; i++) {
       try {
         data.artists.genres.forEach((g) => genres.push(g));
@@ -24,23 +111,14 @@ const Track = () => {
         console.log("No genres found.");
       }
     }
-  }
+    */
 
-  const renderGenres = () => {
-    getTrack();
-
-    if (genres.length === 0) {
-      return (
-        <div>No genres found.</div>
-      )
-    }
-
+  if (genres.length === 0) {
     return (
-      genres.map(genre => (
-        <div>
-          <h2>{ genre }</h2>
-        </div>
-      ))
+      <div class="bg-gray-900 h-full flex-col content-center text-white">
+        <NavBar />
+        <div>No genres found.</div>
+      </div>
     );
   }
 
@@ -48,7 +126,14 @@ const Track = () => {
     <div class="bg-gray-900 h-full flex-col content-center">
       <NavBar />
       <div class="text-white">
-        { renderGenres() }
+        { genres.length === 0 ?
+            <div>No genres found.</div>
+            : genres.map(genre => (
+              <div>
+                <h2>{ genre }</h2>
+              </div>
+            ))
+        }
       </div>
     </div>
   );
