45c49da874314eff9711ca73ef8070136c9b2d63
Tu-An Nguyen <t.nhunhu.ng@gmail.com>
2023-12-12 16:37:13 -0800

    Remove App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import './App.css';
 import Home from "./components/Home";
 import About from "./components/About";
 import Track from "./components/Track";
