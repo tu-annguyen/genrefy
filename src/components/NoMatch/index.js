7a2f5d4695411e41430318c25bab5ac08a8d059e
Tu-An Nguyen <t.nhunhu.ng@gmail.com>
2024-04-15 16:58:04 -0700

    Formatting

--- src/components/NoMatch/index.js
+++ src/components/NoMatch/index.js
@@ -4,9 +4,11 @@ const NoMatch = () => {
   return (
     <div class="bg-gray-900 h-full flex-col content-center">
       <NavBar />
-      <div class="h-screen text-white">
-        <h2 class="font-bold text-3xl my-5">404: Invalid path</h2>
-        <a href="/" class="bg-green-500 text-white font-medium py-2 px-4 my-5 rounded">Return Home</a>
+      <div class="flex justify-center items-center">
+        <div>
+          <h1 class="text-white font-medium text-3xl m-5">404: Invalid URL</h1>
+          <a href="/" class="bg-green-500 text-white font-medium py-2 px-4 mx-16 rounded">Return Home</a>
+        </div>
       </div>
     </div>
   );
