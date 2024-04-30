0150d426c21f7bcc5b77b9debd4a9da86f2af568
Tu-An Nguyen <t.nhunhu.ng@gmail.com>
2024-04-30 15:57:26 -0700

    Remove Track component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-import Track from "./components/Track";
 import NoMatch from "./components/NoMatch";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/track/:id" element={<Track />} />
         <Route path="*" element={<NoMatch />} />
       </Routes> 
     </div>
